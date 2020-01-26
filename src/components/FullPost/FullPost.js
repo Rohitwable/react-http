557c1404350c7f8db645021b907820e230f7d251
Rohitwable <rohitwable46@gmail.com>
2020-01-26 15:22:02 +0530

    stop multiple api calls in componentDidMount

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -8,14 +8,17 @@ class FullPost extends Component {
     }
     componentDidUpdate() {
         if (this.props.id) {
-            axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
-                .then((response) => {
-                    console.log(response);
-                    this.setState({ loadedPost: response.data });
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
+            // stops calling infinite api calls for already loaded post
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then((response) => {
+                        console.log(response);
+                        this.setState({ loadedPost: response.data });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            }
         }
     }
     render() {
