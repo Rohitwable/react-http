b6f6a6b1311cfefebc25eae87a51288710919582
Rohitwable <rohitwable46@gmail.com>
2020-01-26 14:38:27 +0530

    response stored in state / mapped through the data and rendered on screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,22 +7,30 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+    }
     componentDidMount() {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then((result) => {
-                console.log(result)
+                console.log(result);
+                this.setState({ posts: result.data })
             })
             .catch((error) => {
                 console.log(error)
             })
     }
     render() {
+        const posts = this.state.posts.map((post, index) => (
+            <Post
+                title={post.title}
+                key={post.id}
+            />
+        ))
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
