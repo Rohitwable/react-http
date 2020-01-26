90da0d8f16f5ada37a3672904a7044f24d87938d
Rohitwable <rohitwable46@gmail.com>
2020-01-26 14:32:35 +0530

    axios get request

---
+++ README.md
@@ -0,0 +1,3 @@
+axios - we used for making http req to the server
+
+componentDidMount()- used to make http req
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,9 +4,19 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    render() {
         return (
             <div>
                 <section className="Posts">
